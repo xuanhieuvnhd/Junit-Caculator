701a932d41aec642776eef6130b3db82ba26332d
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-08-16 11:13:46 +0700

    16/8

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
